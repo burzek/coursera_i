4db8e1e3939fb912a37f8aedc2e9dae94f5a5422
jerzy_burzek <boris.brinza@gmail.com>
2014-03-18 22:16:17 +0100

    Solver changes

--- puzzle/src/Solver.java
+++ puzzle/src/Solver.java
@@ -1,16 +1,38 @@
 public class Solver {
+
+	private class Node {
+		private int moves;
+		private Board previousBoard;
+		private Board currentBoard;
+
+		private Node(Board board) {
+			moves = 0;
+			previousBoard = null;
+			currentBoard = board;
+		}
+	}
+
+	private MinPQ<Node> queue;
+
+
 	public Solver(Board initial) {
-
+		queue = new MinPQ<Node>();
+		Node n = new Node(initial);
+		queue.insert(n);
 	}
+
 	public boolean isSolvable() {
-
+		return false;
 	}
+
 	public int moves() {
-
+		return 0;
 	}
+
 	public Iterable<Board> solution() {
-
+		return null;
 	}
+
 	public static void main(String[] args) {
 
 	}
