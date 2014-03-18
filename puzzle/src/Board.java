5580ea9af80e48da05dea79dc00966335c259490
jerzy_burzek <boris.brinza@gmail.com>
2014-03-18 23:05:13 +0100

    Solver changes

--- puzzle/src/Board.java
+++ puzzle/src/Board.java
@@ -1,16 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
-	private short[] blocks;
+	private int[] blocks;
 	private int size;
 
 	public Board(int[][] blocks) {
 		size = blocks.length;
-		this.blocks = new short[size * size];
+		this.blocks = new int[size * size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				this.blocks[i * size + j] = blocks[i][j];
+			}
+		}
 	}
 
-	private short get(int row, int col) {
-		return blocks[row * size + col];
-	}
 
 	public int dimension() {
 		return size;
@@ -41,7 +46,35 @@ public class Board {
 		}
 		return true;
 	}
+
+
+	private int[][] convertBoard() {
+		int[][] newBlocks = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				newBlocks[i][j] = this.blocks[i * size + j];
+			}
+		}
+		return  newBlocks;
+	}
+
 	public Board twin() {
+		int[][] newBlocks = convertBoard();
+
+		int row = StdRandom.uniform(size);
+		int fromCol = 0;
+		if (newBlocks[row][fromCol] == 0) {
+			fromCol++;
+		}
+		int toCol = fromCol + 1;
+		if (newBlocks[row][toCol] == 0) {
+			toCol++;
+		}
+		int tmp = newBlocks[row][fromCol];
+		newBlocks[row][fromCol] = newBlocks[row][toCol];
+		newBlocks[row][toCol] = tmp;
+
+		return new Board(newBlocks);
 
 	}
 
@@ -63,7 +96,48 @@ public class Board {
 		return true;
 	}
 
+	private int[][] swapEmpty(int[][] blocks, int row0, int col0, int rowTo, int colTo) {
+		blocks[row0][col0] = blocks[rowTo][colTo];
+		blocks[rowTo][colTo] = 0;
+		return blocks;
+	}
+
 	public Iterable<Board> neighbors() {
+		List<Board> boards = new ArrayList<Board>();
+
+		int[][] blocks = convertBoard();
+		int pos = 0;
+		while (this.blocks[pos] != 0) {
+			pos++;
+		}
+		int row0 = pos / size;
+		int col0 = pos % size;
+
+		//left
+		if (col0 != 0) {
+			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0, col0 - 1);
+			boards.add(new Board(swapBlocks));
+
+		}
+		//right
+		if (col0 != size - 1) {
+			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0, col0 + 1);
+			boards.add(new Board(swapBlocks));
+		}
+		//up
+		if (row0 != 0) {
+			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0 - 1, col0);
+			boards.add(new Board(swapBlocks));
+		}
+		//down
+		if (row0 != size - 1) {
+			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0 + 1, col0);
+			boards.add(new Board(swapBlocks));
+		}
+
+
+		return boards;
+
 
 	}
 
@@ -72,10 +146,10 @@ public class Board {
 		str.append("\r\n");
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				str.append(blocks[i][j]).append(" " );
+				str.append(blocks[i * size + j]).append(" " );
 			}
 			str.append("\r\n");
 		}
-
+		return str.toString();
 	}
 }
--- puzzle/src/Solver.java
+++ puzzle/src/Solver.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.List;
+
 public class Solver {
 
 	private class Node {
@@ -5,35 +8,78 @@ public class Solver {
 		private Board previousBoard;
 		private Board currentBoard;
 
-		private Node(Board board) {
-			moves = 0;
-			previousBoard = null;
-			currentBoard = board;
+		private Node(Board board, Board previousBoard, int moves) {
+			this.moves = moves;
+			this.previousBoard = previousBoard;
+			this.currentBoard = board;
 		}
 	}
 
 	private MinPQ<Node> queue;
+	private List<Board> solution;
 
 
 	public Solver(Board initial) {
 		queue = new MinPQ<Node>();
-		Node n = new Node(initial);
+		Node n = new Node(initial, null, 0);
 		queue.insert(n);
+
+		do {
+			Node node = queue.delMin();
+			solution.add(node.currentBoard);
+			if (node.currentBoard.isGoal()) {
+				break;
+			}
+
+			Iterator<Board> neighbors = node.currentBoard.neighbors().iterator();
+			while (neighbors.hasNext()) {
+				Board b = neighbors.next();
+				if (b.equals(node.previousBoard)) {
+					continue;	//optimization
+				}
+				queue.insert(new Node(b, node.currentBoard, node.moves + 1));
+			}
+
+		} while (!queue.isEmpty());
 	}
 
 	public boolean isSolvable() {
-		return false;
+		return true;
 	}
 
 	public int moves() {
-		return 0;
+		return solution.size();
 	}
 
 	public Iterable<Board> solution() {
-		return null;
+		return solution;
 	}
 
 	public static void main(String[] args) {
+		// create initial board from file
+		In in = new In(args[0]);
+		int N = in.readInt();
+		int[][] blocks = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				blocks[i][j] = in.readInt();
+			}
+		}
 
+
+		Board initial = new Board(blocks);
+
+		// solve the puzzle
+		Solver solver = new Solver(initial);
+
+		// print solution to standard output
+		if (!solver.isSolvable()) {
+			StdOut.println("No solution possible");
+		} else {
+			StdOut.println("Minimum number of moves = " + solver.moves());
+			for (Board board : solver.solution()) {
+				StdOut.println(board);
+			}
+		}
 	}
 }
