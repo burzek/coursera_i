a7ffbb43432fcde3052ee162c9c248ae84a11cea
jerzy_burzek <boris.brinza@gmail.com>
2014-03-27 00:08:41 +0100

    20140327

--- puzzle/src/Board.java
+++ puzzle/src/Board.java
@@ -1,155 +1,180 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Board {
 
-	private int[] blocks;
-	private int size;
+    private int[] blocks;
+    private int size;
 
-	public Board(int[][] blocks) {
-		size = blocks.length;
-		this.blocks = new int[size * size];
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				this.blocks[i * size + j] = blocks[i][j];
+    public Board(int[][] blocks) {
+        size = blocks.length;
+        this.blocks = new int[size * size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                this.blocks[i * size + j] = blocks[i][j];
+            }
+        }
+    }
+
+
+    public int dimension() {
+        return size;
+    }
+
+    public int hamming() {
+        int hamming = 0;
+        for (int pos = 0; pos < size * size; pos++) {
+            if (blocks[pos] != 0 && blocks[pos] != pos + 1) {
+                hamming++;
+            }
+        }
+        return hamming;
+    }
+
+    public int manhattan() {
+        int manhattan = 0;
+        for (int pos = 0; pos < size * size; pos++) {
+			if (blocks[pos] == 0) {
+				continue;
+			}
+
+			int posNeeded = blocks[pos] - 1;
+			if (posNeeded != pos) {
+				int col = pos % size;
+				int row = pos / size;
+
+				int colNeeded = posNeeded % size;
+				int rowNeeded = posNeeded / size;
+
+				int diff = col - colNeeded;
+				if (diff < 0) {
+					diff = -diff;
+				}
+
+				manhattan += diff;
+
+				diff = row - rowNeeded;
+				if (diff < 0) {
+					diff = -diff;
+				}
+
+				manhattan += diff;
+
 			}
 		}
-	}
+        return  manhattan;
+    }
+
+    public boolean isGoal() {
+        for (int i = 0; i < size * size - 1; i++) {
+            if (blocks[i] != i + 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
-	public int dimension() {
-		return size;
-	}
-	public int hamming() {
-		int hamming = 0;
-		for (int pos = 0; pos < size * size - 1; pos++) {
-			hamming += (blocks[pos] != pos + 1 ? 1 : 0);
-		}
-		return hamming + 0;	//todo
-	}
+    private int[][] convertBoard(int[] blocksParam) {
+        int[][] newBlocks = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                newBlocks[i][j] = blocksParam[i * size + j];
+            }
+        }
+        return newBlocks;
+    }
 
-	public int manhattan() {
-		int manhattan = 0;
-		for (int pos = 0; pos < size * size - 1; pos++) {
-			int diff = blocks[pos] - (pos + 1);
-			diff = diff < 0 ? -diff : diff;
-			manhattan += ((diff / size) + (diff % size));
-		}
-		return  manhattan;
-	}
+    public Board twin() {
+        int[] twin = Arrays.copyOf(blocks, size * size);
+        int row = 0;
+        if (twin[row * size] == 0 || twin[row * size + 1] == 0) {
+            row++;
+        }
 
-	public boolean isGoal() {
-		for (int i = 0; i < size * size - 1; i++) {
-			if (blocks[i] != i + 1) {
-				return false;
-			}
-		}
-		return true;
-	}
+        int tmp = this.blocks[row * size];
+        twin[row * size] = twin[row * size + 1];
+        twin[row * size + 1] = tmp;
 
+        return new Board(convertBoard(twin));
 
-	private int[][] convertBoard() {
-		int[][] newBlocks = new int[size][size];
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				newBlocks[i][j] = this.blocks[i * size + j];
-			}
-		}
-		return  newBlocks;
-	}
+    }
 
-	public Board twin() {
-		int[][] newBlocks = convertBoard();
+    public boolean equals(Object y) {
+        if (this == y) {
+            return true;
+        }
+        if (!(y instanceof  Board)) {
+            return false;
+        }
 
-		int row = StdRandom.uniform(size);
-		int fromCol = 0;
-		if (newBlocks[row][fromCol] == 0) {
-			fromCol++;
-		}
-		int toCol = fromCol + 1;
-		if (newBlocks[row][toCol] == 0) {
-			toCol++;
-		}
-		int tmp = newBlocks[row][fromCol];
-		newBlocks[row][fromCol] = newBlocks[row][toCol];
-		newBlocks[row][toCol] = tmp;
+        Board other = (Board) y;
+        for (int i = 0; i < size * size; i++) {
+            if (blocks[i] != other.blocks[i]) {
+                return false;
+            }
+        }
 
-		return new Board(newBlocks);
+        return true;
+    }
 
-	}
+    private int[] swapEmpty(int[] blocksParam, int row0, int col0, int rowTo, int colTo) {
+        int[] swapped = Arrays.copyOf(blocksParam, size * size);
+        int idxFrom = row0 * size + col0;
+        int idxTo = rowTo * size + colTo;
+        swapped[idxFrom] = swapped[idxTo];
+        swapped[idxTo] = 0;
+        return swapped;
+    }
 
-	public boolean equals(Object y) {
-		if (this == y) {
-			return true;
-		}
-		if (!(y instanceof  Board)) {
-			return false;
-		}
+    public Iterable<Board> neighbors() {
+        List<Board> boards = new ArrayList<Board>();
 
-		Board other = (Board) y;
-		for (int i = 0; i < size * size; i++) {
-			if (blocks[i] != other.blocks[i]) {
-				return false;
-			}
-		}
+        int pos = 0;
+        while (this.blocks[pos] != 0) {
+            pos++;
+        }
+        int row0 = pos / size;
+        int col0 = pos % size;
 
-		return true;
-	}
+        //left
+        if (col0 != 0) {
+            int[] swapBlocks = swapEmpty(this.blocks, row0, col0, row0, col0 - 1);
+            boards.add(new Board(convertBoard(swapBlocks)));
 
-	private int[][] swapEmpty(int[][] blocks, int row0, int col0, int rowTo, int colTo) {
-		blocks[row0][col0] = blocks[rowTo][colTo];
-		blocks[rowTo][colTo] = 0;
-		return blocks;
-	}
-
-	public Iterable<Board> neighbors() {
-		List<Board> boards = new ArrayList<Board>();
-
-		int[][] blocks = convertBoard();
-		int pos = 0;
-		while (this.blocks[pos] != 0) {
-			pos++;
-		}
-		int row0 = pos / size;
-		int col0 = pos % size;
-
-		//left
-		if (col0 != 0) {
-			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0, col0 - 1);
-			boards.add(new Board(swapBlocks));
-
-		}
-		//right
-		if (col0 != size - 1) {
-			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0, col0 + 1);
-			boards.add(new Board(swapBlocks));
-		}
-		//up
-		if (row0 != 0) {
-			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0 - 1, col0);
-			boards.add(new Board(swapBlocks));
-		}
-		//down
-		if (row0 != size - 1) {
-			int[][] swapBlocks = swapEmpty(blocks, row0, col0, row0 + 1, col0);
-			boards.add(new Board(swapBlocks));
-		}
+        }
+        //right
+        if (col0 != size - 1) {
+            int[] swapBlocks = swapEmpty(this.blocks, row0, col0, row0, col0 + 1);
+            boards.add(new Board(convertBoard(swapBlocks)));
+        }
+        //up
+        if (row0 != 0) {
+            int[] swapBlocks = swapEmpty(this.blocks, row0, col0, row0 - 1, col0);
+            boards.add(new Board(convertBoard(swapBlocks)));
+        }
+        //down
+        if (row0 != size - 1) {
+            int[] swapBlocks = swapEmpty(this.blocks, row0, col0, row0 + 1, col0);
+            boards.add(new Board(convertBoard(swapBlocks)));
+        }
 
 
-		return boards;
+        return boards;
 
 
-	}
+    }
 
-	public String toString() {
-		StringBuilder str = new StringBuilder(size);
-		str.append("\r\n");
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				str.append(blocks[i * size + j]).append(" " );
-			}
-			str.append("\r\n");
-		}
-		return str.toString();
-	}
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(size + "\n");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                s.append(String.format("%2d ", this.blocks[i * size + j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 }
--- puzzle/src/Solver.java
+++ puzzle/src/Solver.java
@@ -1,85 +1,124 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solver {
 
-	private class Node {
-		private int moves;
-		private Board previousBoard;
-		private Board currentBoard;
+    private class Node implements Comparable {
+        private int moves;
+        private Node previousNode;
+        private Board currentBoard;
 
-		private Node(Board board, Board previousBoard, int moves) {
-			this.moves = moves;
-			this.previousBoard = previousBoard;
-			this.currentBoard = board;
-		}
-	}
+        private Node(Board board, Node previousNode, int moves) {
+            this.moves = moves;
+            this.previousNode = previousNode;
+            this.currentBoard = board;
+        }
 
-	private MinPQ<Node> queue;
-	private List<Board> solution;
+        @Override
+        public int compareTo(Object o) {
+            Node other = (Node) o;
+            return (this.currentBoard.manhattan() + moves) - (other.currentBoard.manhattan() + other.moves);
+        }
+    }
 
+    private MinPQ<Node> queue;
+    private MinPQ<Node> queueTwin;
+    private boolean isSolvable;
 
-	public Solver(Board initial) {
-		queue = new MinPQ<Node>();
-		Node n = new Node(initial, null, 0);
-		queue.insert(n);
+    private List<Board> solutionList;
 
-		do {
-			Node node = queue.delMin();
-			solution.add(node.currentBoard);
-			if (node.currentBoard.isGoal()) {
-				break;
-			}
+    public Solver(Board initial) {
+        Node solutionNode = null;
 
-			Iterator<Board> neighbors = node.currentBoard.neighbors().iterator();
-			while (neighbors.hasNext()) {
-				Board b = neighbors.next();
-				if (b.equals(node.previousBoard)) {
-					continue;	//optimization
-				}
-				queue.insert(new Node(b, node.currentBoard, node.moves + 1));
-			}
+        queue = new MinPQ<Node>();
+        queueTwin = new MinPQ<Node>();
 
-		} while (!queue.isEmpty());
-	}
+        queue.insert(new Node(initial, null, 0));
+        queueTwin.insert(new Node(initial.twin(), null, 0));
 
-	public boolean isSolvable() {
-		return true;
-	}
+        while (!isSolvable) {
+            Node node = queue.delMin();
+            Node nodeTwin = queueTwin.delMin();
 
-	public int moves() {
-		return solution.size();
-	}
+            if (nodeTwin.currentBoard.isGoal()) {
+                break;
+            }
 
-	public Iterable<Board> solution() {
-		return solution;
-	}
+            if (node.currentBoard.isGoal()) {
+                solutionNode = node;
+                isSolvable = true;
+            }
 
-	public static void main(String[] args) {
-		// create initial board from file
-		In in = new In(args[0]);
-		int N = in.readInt();
-		int[][] blocks = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				blocks[i][j] = in.readInt();
-			}
-		}
+            processNeighbors(queue, node);
+            processNeighbors(queueTwin, nodeTwin);
 
+        }
 
-		Board initial = new Board(blocks);
+        //
+        solutionList = new LinkedList<Board>();
+        if (isSolvable) {
+            while (solutionNode != null) {
+                solutionList.add(0, solutionNode.currentBoard);
+                solutionNode = solutionNode.previousNode;
+            }
+        }
 
-		// solve the puzzle
-		Solver solver = new Solver(initial);
+    }
 
-		// print solution to standard output
-		if (!solver.isSolvable()) {
-			StdOut.println("No solution possible");
+    public boolean isSolvable() {
+        return isSolvable;
+    }
+
+    public int moves() {
+        return solutionList.size();
+    }
+
+    public Iterable<Board> solution() {
+        if (!isSolvable) {
+			return null;
 		} else {
-			StdOut.println("Minimum number of moves = " + solver.moves());
-			for (Board board : solver.solution()) {
-				StdOut.println(board);
-			}
+			return solutionList;
 		}
-	}
+    }
+
+
+    private void processNeighbors(MinPQ<Node> queueParam, Node node) {
+        Iterator<Board> neighbors = node.currentBoard.neighbors().iterator();
+        while (neighbors.hasNext()) {
+            Board b = neighbors.next();
+            if (node.previousNode != null && b.equals(node.previousNode.currentBoard)) {
+                continue;	//optimization
+            }
+            queueParam.insert(new Node(b, node, node.moves + 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                blocks[i][j] = in.readInt();
+            }
+        }
+
+
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable()) {
+            StdOut.println("No solution possible");
+        } else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution()) {
+                StdOut.println(board);
+            }
+        }
+    }
 }
